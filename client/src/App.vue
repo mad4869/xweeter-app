96a85980736d9d6cb5c447501986bb2317a8e98f
MAkbarD <mad4869@gmail.com>
2023-09-01 13:52:17 +0700

    Added new zweet

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
+import NewZweet from './components/NewZweet.vue';
 </script>
 
 <template>
   <Navbar />
+  <NewZweet />
 </template>
 
 <style scoped></style>
---
+++ client/src/components/NewZweet.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section
+        class="w-3/5 mx-auto mt-4 px-12 flex flex-col justify-between items-center border border-solid border-sky-800 rounded-xl">
+        <span class="w-full py-4 text-2xl text-white">New Zweet</span>
+        <textarea name="new-tweet" id="new-tweet" rows="1" placeholder="Tell them what have you been up to..."
+            spellcheck="false"
+            class="w-full px-4 py-2 bg-slate-200 rounded-lg resize-none transition-transform duration-100 ease-out placeholder:text-slate-400 focus:h-32 focus:outline-none focus-visible:outline-sky-600"></textarea>
+        <div class="w-full py-4 flex justify-between items-start">
+            <label for="add-image">
+                <span class="px-2 py-1 bg-slate-400 text-xs text-white rounded-md cursor-pointer">Add Image</span>
+                <input type="file" id="add-image" alt="Add Image" accept="image/jpeg, image/png" multiple class="hidden">
+            </label>
+            <input type="submit" value="Zweet"
+                class="px-4 py-1 bg-sky-600 text-white font-semibold rounded-md cursor-pointer">
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
