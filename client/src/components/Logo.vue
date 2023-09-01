5dfaa30a049191e8596e7cdb7fc2900063caf1bd
MAkbarD <mad4869@gmail.com>
2023-09-01 10:44:39 +0700

    Fixed navbar

---
+++ client/src/components/Logo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{ size: 'w-8' | 'w-16' | 'w-32' }>()
+</script>
+
+<template>
+    <img :class="props.size" src="../assets/logo.png" loading="lazy">
+</template>
+
+<style scoped></style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-
+import Logo from './Logo.vue';
 </script>
 
 <template>
     <nav
-        class="grid grid-cols-4 place-items-center w-4/5 mx-auto mt-4 px-20 py-4 bg-sky-600 ring-4 ring-sky-800 text-white rounded-full">
+        class="grid grid-cols-4 place-items-center mx-auto px-20 py-4 text-white border-b border-solid border-sky-600 shadow-md shadow-sky-600/50">
         <div class="col-span-1"></div>
         <div class="col-span-2 flex justify-center items-center">
-            <h1 class="text-5xl">Z</h1>
+            <Logo size="w-8" />
         </div>
         <ul class="col-span-1 flex justify-center items-center gap-4">
             <li>Profile</li>
